48e98ef08b5af3b7c51066861f8964dbdeb570f2
tiamo405 <nam05052002@gmail.com>
2026-06-30 14:55:50 +0700

    ac

---
+++ 263.ugly-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        while n > 1:
+            if n % 2 == 0:
+                n /= 2
+            elif n % 3 == 0:
+                n /= 3
+            elif n % 5 == 0:
+                n /= 5
+            else:
+                return False
+        return n == 1
---
+++ 268.missing-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        return len(nums)
