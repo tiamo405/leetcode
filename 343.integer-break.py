cb505cbc50f229f743e6e7c9b357131d5035fe6a
tiamo405 <nam05052002@gmail.com>
2026-07-27 15:55:11 +0700

    ai-ac

---
+++ 343.integer-break.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+        res = 1
+        while n > 4:
+            res *= 3
+            n -= 3
+        return res * n
